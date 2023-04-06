baf25613ffcc935afeeb55996616d94b1f70fcab
nilasruthy <77112312+nilasruthy@users.noreply.github.com>
2023-04-06 13:26:20 +0530

    Delete UseCase directory

--- UseCase/src/main/java/com/example/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.model.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long>{
-
-}
